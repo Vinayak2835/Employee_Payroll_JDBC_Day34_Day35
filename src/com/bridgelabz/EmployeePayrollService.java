d39041977b54ffa44a5831cf62da7dffcfdd9d02
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-06-01 14:54:09 +0530

    [Vinayak] Add: Service class for EmployeeWage is added

--- src/com/bridgelabz/EmployeePayrollService.java
+++ src/com/bridgelabz/EmployeePayrollService.java
@@ -27,7 +27,7 @@ public class EmployeePayrollService {
 
     public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
         this();
-        this.employeePayrollList = employeePayrollList;
+         this.employeePayrollList = employeePayrollList;
     }
 
     public static void main(String[] args) {
